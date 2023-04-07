d814713ebe9fed5200b3bf67d609e741f4455926
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-04-07 09:32:23 +0700

    include

---
+++ easd_1_156_D/easd_1_156_D.cpp
@@ -0,0 +1,21 @@
+//1.untuk menyelesaikan suatu masalah karena algoritma merupakan yang di rancang untuk menylesaikan masalah
+//2.linear data structure dan non-linear data structure
+//3.ukuran input:semakin besar ukuran input maka semkain  lama juga waktu yang di perluhkan,proceceor:procecor juga sangat mempergaruhi ke efisienan sutau kinerja lagoritma semkain bagus besar maka bisa semakin cepat,penggunaan memory:memory juga bisa mmepergaruhi efiensi program algoritma jika pemrgorman menggunakan memory secara berlebihan maka akan memperlambat kinerja sutau pemgroman
+//4.selection sort
+//5.Quadratic (O(n^2)):
+//Bubble Sort
+//Selection Sort
+//Insertion Sort
+//Algoritma - algoritma di atas memiliki kompleksitas waktu yang bersifat kuadratik, yang berarti waktu eksekusinya meningkat secara kuadratik seiring dengan bertambahnya ukuran input data.Oleh karena itu, algoritma - algoritma ini kurang efisien untuk digunakan pada data dengan ukuran yang besar
+//Loglinear(O(n log n)) :
+//Merge Sort
+//Quick Sort
+//Heap Sort
+//Algoritma - algoritma di atas memiliki kompleksitas waktu yang bersifat loglinear, yang berarti waktu eksekusinya meningkat secara logaritmik seiring dengan bertambahnya ukuran input data.Algoritma - algoritma ini lebih efisien dibandingkan dengan algoritma Quadratic pada data dengan ukuran yang besar.
+
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
